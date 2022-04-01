011b520188e4eca3b605652e9336f895cf7401f2
Arushi-goyal <arushigoyal99@gmail.com>
2022-04-01 15:00:27 +0530

    first commit

---
+++ Js/script.js
@@ -0,0 +1,11 @@
+const hamberger = document.querySelector(".humberger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamberger.addEventListener("click", () => {
+    hamberger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamberger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
